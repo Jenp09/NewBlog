0003112f82d3d6738a84b0810bd44c739e6802eb
Jenp09 <35925345+Jenp09@users.noreply.github.com>
2019-01-04 20:49:32 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function Random()  {
+    "use strict" ;
+  $("#switch-view").click(function(){
+       $(".article-wrapper").toggleClass("bloc col-xs-12 col-xs-4");
+    $(this).find("button").toggleClass("active");
+   
+  }
+)};
+
+Random();
